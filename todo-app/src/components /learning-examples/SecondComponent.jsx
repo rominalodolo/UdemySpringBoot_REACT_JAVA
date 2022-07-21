4a5c54bc28474caada7e9527c82afc35e8161c4a
Romina <lodoloromina@gmail.com>
2022-07-21 15:09:33 +0200

    update

---
+++ todo-app/src/components /learning-examples/SecondComponent.jsx
@@ -0,0 +1,14 @@
+// Class Component 
+
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+export default class FirstComponent extends Component {
+    render() {
+        return (
+            <div className="firstComponent">
+            FirstComponent
+            </div>
+        );
+    }
+}
