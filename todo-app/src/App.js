b29b5c1366ef2da390f564d358f012411e820fb9
Romina <lodoloromina@gmail.com>
2022-07-22 16:07:35 +0200

    packages

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Counter from './components/counter/Counter'
+// import Counter from './components/counter/Counter'
 import './App.css';
+import TodoApp from './components/todo/TodoApp'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        {/* <Counter /> */}
+        <TodoApp />
       </div>
     );
   }
