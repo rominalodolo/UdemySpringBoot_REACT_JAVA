611e93c1cef7734221a6742f6a0959e2f85b0a61
Romina <lodoloromina@gmail.com>
2022-07-25 14:48:21 +0200

    update

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -9,9 +9,11 @@ import FooterComponent from './FooterComponent.jsx'
 import LogoutComponent from './LogoutComponent.jsx'
 import WelcomeComponent from './WelcomeComponent.jsx'
 import TodoComponent from './TodoComponent.jsx'
+import withNavigation from './WithNavigation.jsx'   
 
 class TodoApp extends Component {
     render() {
+        const LoginComponentWithNavigation = withNavigation(LoginComponent);
         return (
             <div className="TodoApp">
                 <Router>
@@ -19,7 +21,7 @@ class TodoApp extends Component {
                         <HeaderComponent/>
                         <Routes>
                             <Route path="/welcome" exact component={ <LoginComponent/> } />
-                            <Route path="/login" component={LoginComponent}/>
+                            <Route path="/login" component={LoginComponent}/> 
                             <AuthenticatedRoute path="/welcome/:name" component={WelcomeComponent}/>
                             <AuthenticatedRoute path="/todos/:id" component={TodoComponent}/>
                             <AuthenticatedRoute path="/todos" component={ListTodosComponent}/>
