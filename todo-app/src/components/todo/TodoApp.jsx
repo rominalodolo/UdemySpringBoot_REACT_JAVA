7c0c8c215c2cb959a4a176fdf15bac99870571ea
Romina <lodoloromina@gmail.com>
2022-07-25 21:44:33 +0200

    update

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,46 +1,122 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import withNavigation from './WithNavigation.jsx'	
+import withParams from './WithParams.jsx'
+
 import AuthenticatedRoute from './AuthenticatedRoute.jsx'
 import LoginComponent from './LoginComponent.jsx'
-import ListTodosComponent from './ListTodosComponent.jsx'
-import ErrorComponent from './ErrorComponent.jsx'
 import HeaderComponent from './HeaderComponent.jsx'
 import FooterComponent from './FooterComponent.jsx'
+import ErrorComponent from './ErrorComponent.jsx'
 import LogoutComponent from './LogoutComponent.jsx'
 import WelcomeComponent from './WelcomeComponent.jsx'
+import ListTodosComponent from './ListTodosComponent.jsx'
 import TodoComponent from './TodoComponent.jsx'
-import withNavigation from './WithNavigation.jsx'   
 
 class TodoApp extends Component {
     render() {
-        const LoginComponentWithNavigation = withNavigation(LoginComponent);
+    	const LoginComponentWithNavigation = withNavigation(LoginComponent);
+
+    	const WelcomeComponentWithParams = withParams(WelcomeComponent);
+
+    	const HeaderComponentWithNavigation = withNavigation(HeaderComponent);
+
+      //REACT-6
+    	const TodoComponentWithParamsAndNavigation = withParams(withNavigation(TodoComponent));
+
+      //REACT-6
+    	const ListTodosComponentWithNavigation = withNavigation(ListTodosComponent) 
+
         return (
             <div className="TodoApp">
-                <Router>
-                    <>
-                        <HeaderComponent/>
-                        <Routes>
-                            <Route path="/welcome" exact component={ <LoginComponent/> } />
-                            <Route path="/login" element={<LoginComponentWithNavigation />} />
-                            {/* <Route path="/login" component={LoginComponent}/>  */}
-                            <AuthenticatedRoute path="/welcome/:name" component={WelcomeComponent}/>
-                            <AuthenticatedRoute path="/todos/:id" component={TodoComponent}/>
-                            <AuthenticatedRoute path="/todos" component={ListTodosComponent}/>
-                            <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
+            	<Router>
+            		<HeaderComponentWithNavigation/>
+            		<Routes>
+		            	<Route path="/" element={<LoginComponentWithNavigation />} />
+		            	<Route path="/login" element={<LoginComponentWithNavigation />} />
+		            	<Route path="/welcome/:name" element={
+		            		<AuthenticatedRoute>
+		            			<WelcomeComponentWithParams />
+		            		</AuthenticatedRoute>
+		            	} />
+		            	
+		            	{/* /REACT-6 */}
+		            	<Route path="/todos/:id" element={ 
+			            	<AuthenticatedRoute>
+			            		<TodoComponentWithParamsAndNavigation />
+			            	</AuthenticatedRoute>
+			            } />
 
-                            <Route component={ErrorComponent}/>
-                        </Routes>
-                        <FooterComponent/>
-                    </>
-                </Router>
-                {/*<LoginComponent/>
-                <WelcomeComponent/>*/}
+			            {/* REACT-6 */}
+		            	<Route path="/todos" element={
+			            	<AuthenticatedRoute>
+			            		<ListTodosComponentWithNavigation /> 
+			            	</AuthenticatedRoute>
+			            } />
+		            	
+                  <Route path="/logout" element={
+		            		<AuthenticatedRoute>
+		            			<LogoutComponent />
+		            		</AuthenticatedRoute>
+		            	} />
+		            	<Route path="*" element={<ErrorComponent />} />
+	            	</Routes>
+	            	<FooterComponent/>
+            	</Router>
             </div>
         )
     }
 }
 
 export default TodoApp
+
+
+
+// import React, {Component} from 'react'
+// import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import AuthenticatedRoute from './AuthenticatedRoute.jsx'
+// import LoginComponent from './LoginComponent.jsx'
+// import ListTodosComponent from './ListTodosComponent.jsx'
+// import ErrorComponent from './ErrorComponent.jsx'
+// import HeaderComponent from './HeaderComponent.jsx'
+// import FooterComponent from './FooterComponent.jsx'
+// import LogoutComponent from './LogoutComponent.jsx'
+// import WelcomeComponent from './WelcomeComponent.jsx'
+// import TodoComponent from './TodoComponent.jsx'
+// import withNavigation from './WithNavigation.jsx'   
+
+// class TodoApp extends Component {
+//     render() {
+//         const LoginComponentWithNavigation = withNavigation(LoginComponent);
+//         return (
+//             <div className="TodoApp">
+//                 <Router>
+//                     <>
+//                         <HeaderComponent/>
+//                         <Routes>
+//                             <Route path="/welcome" exact component={ <LoginComponent/> } />
+//                             <Route path="/login" element={<LoginComponentWithNavigation />} />
+//                             {/* <Route path="/login" component={LoginComponent}/>  */}
+//                             <AuthenticatedRoute path="/welcome/:name" component={WelcomeComponent}/>
+//                             <AuthenticatedRoute path="/todos/:id" component={TodoComponent}/>
+//                             <AuthenticatedRoute path="/todos" component={ListTodosComponent}/>
+//                             <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
+
+//                             <Route component={ErrorComponent}/>
+//                         </Routes>
+//                         <FooterComponent/>
+//                     </>
+//                 </Router>
+//                 {/*<LoginComponent/>
+//                 <WelcomeComponent/>*/}
+//             </div>
+//         )
+//     }
+// }
+
+// export default TodoApp
 
 
 // import React, {Component} from 'react'
