76cff249e77bd36009b69ea1ceefd79d855f9fb3
Romina <lodoloromina@gmail.com>
2022-08-01 18:39:25 +0200

    update

--- restful-web-services/src/main/java/com/in28minutes/rest/services/restfulwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/services/restfulwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,8 +1,14 @@
 package com.in28minutes.rest.services.restfulwebservices.basic.auth;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,15 +22,3 @@ public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerA
                 .httpBasic();
     }
 }
-
-}
-
-import org.springframework.context.annotation.Configuration;
-        import org.springframework.http.HttpMethod;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter{
