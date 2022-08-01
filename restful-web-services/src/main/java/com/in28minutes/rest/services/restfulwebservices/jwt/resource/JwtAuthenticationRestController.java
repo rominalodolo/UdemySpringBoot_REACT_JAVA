18e30cf163435bb0d4ca4d2adbed4c351f662216
Romina <lodoloromina@gmail.com>
2022-08-01 22:36:03 +0200

    update

--- restful-web-services/src/main/java/com/in28minutes/rest/services/restfulwebservices/jwt/resource/JwtAuthenticationRestController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/services/restfulwebservices/jwt/resource/JwtAuthenticationRestController.java
@@ -1,20 +1,22 @@
 package com.in28minutes.rest.services.restfulwebservices.jwt.resource;
 
-import com.in28minutes.rest.services.restfulwebservices.jwt.AuthenticationException;
+import com.in28minutes.rest.services.restfulwebservices.jwt.resource.AuthenticationException;
 import com.in28minutes.rest.services.restfulwebservices.jwt.JwtTokenUtil;
 import com.in28minutes.rest.services.restfulwebservices.jwt.JwtUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
